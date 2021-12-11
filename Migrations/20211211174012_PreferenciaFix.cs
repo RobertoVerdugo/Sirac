35d31f718f10b085248bed4d409a05ea8958abc0
RobertoVerdugo <roberto.verdugo@userena.cl>
2021-12-11 14:40:47 -0300

    Migracion

---
+++ Migrations/20211211174012_PreferenciaFix.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Test_Razor.Migrations
+{
+    public partial class PreferenciaFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Retiever",
+                table: "Preferencia");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Labrador_Retriever",
+                table: "Preferencia",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Labrador_Retriever",
+                table: "Preferencia");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Retiever",
+                table: "Preferencia",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
